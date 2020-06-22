6eb9f1339840918031e37fe58644e7a2f51bcb8e
BBAS <65233836+BBASBot@users.noreply.github.com>
2020-06-22 13:52:22 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,17 +3,14 @@ module.exports = (client) => {
 
     client.user.setPresence({
         game: {
-            name: "Yippes | v0.1",
+            name: "Yippies | v0.1",
             type: "PLAYING"
         }
     })  
-function setData() {
+
+    function setData() {
         let guild = client.guilds.get("684121060916068393")
-        let memberCount = guild.memberCount
-        let boosterCount = guild.premiumSubscriptionCount
-        guild.channels.get("709793993852190740").setName("Server Members: " + memberCount)
-        guild.channels.get("709794564004904960").setName("Server Boosts: " + boosterCount)
-    console.log(guild);
+        console.log(guild);
     }
-    setInterval(setData, 60000);
+    setInterval(setData, 10000);
 };
