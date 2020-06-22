203ae32a7f90bd52fd1b6cbbe16ed42afc3d9b2e
BBAS <65233836+BBASBot@users.noreply.github.com>
2020-06-22 13:31:33 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -10,7 +10,9 @@ module.exports = (client) => {
 function setData() {
         let guild = client.guilds.get("684121060916068393")
         let memberCount = guild.memberCount
+        let boosterCount = guild.premiumSubscriptionCount
         guild.channels.get("709793993852190740").setName("Server Members: " + memberCount)
+        guild.channels.get("709794564004904960").setName("Server Boosts: " + boosterCount)
     }
     setInterval(setData, 60000);
 };
