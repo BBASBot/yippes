e8d3a8583ec37014c0a8cfa5e607370149af58b8
BBAS <65233836+BBASBot@users.noreply.github.com>
2020-06-23 12:55:31 -0500

    Update bread.js

--- commands/general/bread.js
+++ commands/general/bread.js
@@ -1,11 +1,6 @@
 const { RichEmbed } = require("discord.js");
-const usedCommand = new Set();
 
-module.exports.run = async (bot, message, args) => {
-    if(usedCommand.has(message.author.id)){
-        message.reply('Chill bro, ur on cooldown!')
-    } else {
-        module.exports = {
+module.exports = {
     name: "bread",
     category: "general",
     description: "BREAD BREAD BREAD BREAD AAAAAAAAAA",
@@ -13,11 +8,3 @@ module.exports.run = async (bot, message, args) => {
         const msg = await message.channel.send(":bread:\n:bread:\n:bread:");
     }
 };
-        
-        
-        usedCommand.add(message.author.id);
-        setTimeout(() => {
-            usedCommand.delete(message.author.id);
-        }, 5000); 
-    }
-}
