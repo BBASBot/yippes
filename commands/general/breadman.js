4cf04af1a0b66b6420c7fdab2c4f7fe147b4b298
BBAS <65233836+BBASBot@users.noreply.github.com>
2020-06-24 17:25:44 -0500

    Update breadman.js

--- commands/general/breadman.js
+++ commands/general/breadman.js
@@ -5,6 +5,6 @@ module.exports = {
     category: "general",
     description: "BREAD BREAD BREAD BREAD AAAAAAAAAA",
     run: async(client, message, args) => {
-        const msg = await message.channel.send(":womans_hat:\n:breadflush:\n:shirt:\n:jeans:\n:ballet_shoes:");
+        const msg = await message.channel.send(":womans_hat:\n<:breadflush:725476432372826113>\n:shirt:\n:jeans:\n:ballet_shoes:");
     }
 };
