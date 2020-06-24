2bce815934f3f1feb91d0c60f9009b5db25fd857
BBAS <65233836+BBASBot@users.noreply.github.com>
2020-06-24 17:18:00 -0500

    Create breadman.js

---
+++ commands/general/breadman.js
@@ -0,0 +1,10 @@
+const { RichEmbed } = require("discord.js");
+
+module.exports = {
+    name: "breadman",
+    category: "general",
+    description: "BREAD BREAD BREAD BREAD AAAAAAAAAA",
+    run: async(client, message, args) => {
+        const msg = await message.channel.send(":womans_hat:\n:breadflush:\n:shirt:\n:jeans:\n:ballet_shoes:");
+    }
+};
